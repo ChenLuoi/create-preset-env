43847968a567ec307deaa15705cb214170bc3612
chen <chen@chenluoi.com>
2026-01-15 16:25:50 +0800

    feat: support multiple config file, and add logs

--- env.config.cjs
+++ env.config.cjs
@@ -1,22 +1,21 @@
-{
-    "$schema": "./schema.json",
-    "target": ".env.local",
-    "overrideDescription": true,
-    "variables": {
+module.exports = {
+    target: ".env.local",
+    overrideDescription: true,
+    variables: {
         "API_BASE": "https://api.example.com",
         "CDN_BASE": "https://cdn.example.com",
         "MULTILINE_KEY": "LINE1\nLINE2",
         "OBJECT_KEY": {
-            "value": "object value",
-            "description": "This is a object style definition"
+            value: "object value",
+            description: "This is a object style definition"
         },
         "NUMBER_KEY": {
             "value": 3040,
             "description": "This is a number"
         },
-        "CONFIG_FROM_JSON": {
+        "CONFIG_FROM_CJS": {
             "value": true,
-            "description": "This is config defined in env.config.json"
+            "description": "This is config defined in env.config.cjs"
         }
     }
 }
